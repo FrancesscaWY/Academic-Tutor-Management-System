1bc4639fe27ab71ea2447a43c2bafc8c23629888
FrancesscaWY <3449997118@qq.com>
2024-06-09 10:12:07 +0800

    完成了学生页面显示申请信息，修补了教师显示显示学生申请的bug

--- student_page.php
+++ student_page.php
@@ -1,14 +1,15 @@
-<!--输出导师信息-->
 <?php
-$pwd=file_get_contents('./src/pw.txt');
-$db_host='localhost';
-$db_user='root';
-$db_password=$pwd;
-$db_name='Academic_Tutor_Management_System';
-$conn=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die('Database connection error');
-//将Tutor表中的数据显示出来
-$sql="SELECT * FROM TUTORS";
-$result=mysqli_query($conn,$sql);
+// 获取数据库连接信息
+$pwd = file_get_contents('./src/pw.txt');
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = $pwd;
+$db_name = 'Academic_Tutor_Management_System';
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die('Database connection error');
+
+// 获取导师表中的所有数据
+$sql = "SELECT * FROM TUTORS";
+$result = mysqli_query($conn, $sql);
 echo "<table border='1' id='chose_teacher'>
 <tr>
 <th>序号</th>
@@ -19,88 +20,115 @@ echo "<table border='1' id='chose_teacher'>
 <th>专业</th>
 <th>研究方向</th>
 </tr>";
-while($row=mysqli_fetch_array($result)){
+while ($row = mysqli_fetch_array($result)) {
     echo "<tr>";
-    echo "<td>".$row['T_ID']."</td>";
-    echo "<td>".$row['TNO']."</td>";
-    echo "<td>".$row['TNAME']."</td>";
-    echo "<td>".$row['PROFESSIONAL_TITLE']."</td>";
-    echo "<td>".$row['TSEX']."</td>";
-    echo "<td>".$row['MAJIOR']."</td>";
-    echo "<td>".$row['RESEARCH_FIELD']."</td>";
-//    echo "<td><button>点击查看</button></td>";
+    echo "<td>" . $row['T_ID'] . "</td>";
+    echo "<td>" . $row['TNO'] . "</td>";
+    echo "<td>" . $row['TNAME'] . "</td>";
+    echo "<td>" . $row['PROFESSIONAL_TITLE'] . "</td>";
+    echo "<td>" . $row['TSEX'] . "</td>";
+    echo "<td>" . $row['MAJIOR'] . "</td>";
+    echo "<td>" . $row['RESEARCH_FIELD'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
-mysqli_close($conn);
-?>
-<!--//自动填写学生信息-->
-<?php
-$pwd=file_get_contents('./src/pw.txt');
-$db_host='localhost';
-$db_user='root';
-$db_password=$pwd;
-$db_name='Academic_Tutor_Management_System';
-$conn=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die('Database connection error');
-$sql1="SELECT SNO,SNAME,SDEPT FROM STUDENTS WHERE SNO=".$_COOKIE['copy_account'];
-$result1=mysqli_query($conn,$sql1);
-$row1=mysqli_fetch_assoc($result1);
-$NAME=$row1['SNAME'];
-$DEPT=$row1['SDEPT'];
-$SNO=$row1['SNO'];
-?>
-<!--显示选择进度-->
-<?php
-$pwd=file_get_contents('./src/pw.txt');
-$db_host='localhost';
-$db_user='root';
-$db_password=$pwd;
-$db_name='Academic_Tutor_Management_System';
-$conn=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die('Database connection error');
-//先判断申请是否存入数据库
-$sql="SELECT * FROM S_CHOICE_T WHERE SNO=".$_COOKIE['copy_account'];
-$result=mysqli_query($conn,$sql);
-$application=0;
-if($result){
-    $application=1;
-}
-//再判断是否选择成功
-$sql1="SELECT * FROM T_CHOICE_S WHERE SNO=".$_COOKIE['copy_account'];
-$result1=mysqli_query($conn,$sql1);
-$re_choose=0;
-if($result1){
-    $re_choose=1;
-}
-mysqli_close($conn);
-?>
+mysqli_free_result($result); // 释放结果集
 
+// 获取学生信息
+$sno = $_COOKIE['copy_account'];
+$sql1 = "SELECT SNO, SNAME, SDEPT FROM STUDENTS WHERE SNO = $sno";
+$result1 = mysqli_query($conn, $sql1);
+if ($row1 = mysqli_fetch_assoc($result1)) {
+    $NAME = $row1['SNAME'];
+    $DEPT = $row1['SDEPT'];
+    $SNO = $row1['SNO'];
+} else {
+    die('Student not found.');
+}
+mysqli_free_result($result1); // 释放结果集
+
+// 检查是否提交了导师申请
+$sql2 = "SELECT * FROM S_CHOICE_T WHERE SNO = $sno";
+$result2 = mysqli_query($conn, $sql2);
+
+$application = 0;
+$re_choose = 0;
+$first_tno = '';
+$second_tno = '';
+$third_tno = '';
+$first_tname = '';
+$second_tname = '';
+$third_tname = '';
+$matched_tno = '';
+$matched_tname = '';
+
+if ($row2 = mysqli_fetch_assoc($result2)) {
+    $application = 1; // 表示申请已提交
+    $first_tno = $row2['FRIST_TNO'];
+    $second_tno = $row2['SECOND_TNO'];
+    $third_tno = $row2['THIRD_TNO'];
+
+    // 获取导师的名字
+    $sql_tutors = "SELECT TNO, TNAME FROM TUTORS WHERE TNO IN ('$first_tno', '$second_tno', '$third_tno')";
+    $result_tutors = mysqli_query($conn, $sql_tutors);
+    while ($row_tutor = mysqli_fetch_assoc($result_tutors)) {
+        if ($row_tutor['TNO'] == $first_tno) {
+            $first_tname = $row_tutor['TNAME'];
+        } elseif ($row_tutor['TNO'] == $second_tno) {
+            $second_tname = $row_tutor['TNAME'];
+        } elseif ($row_tutor['TNO'] == $third_tno) {
+            $third_tname = $row_tutor['TNAME'];
+        }
+    }
+    mysqli_free_result($result_tutors); // 释放结果集
+
+    // 检查导师是否选择了该学生
+    $sql3 = "SELECT * FROM T_CHOICE_S WHERE SNO = $sno";
+    $result3 = mysqli_query($conn, $sql3);
+    if ($row3 = mysqli_fetch_assoc($result3)) {
+        $re_choose = 1; // 表示导师已选择该学生
+        $matched_tno = $row3['TNO'];
+
+        // 获取匹配导师的名字
+        $sql_matched_tutor = "SELECT TNAME FROM TUTORS WHERE TNO = '$matched_tno'";
+        $result_matched_tutor = mysqli_query($conn, $sql_matched_tutor);
+        if ($row_matched_tutor = mysqli_fetch_assoc($result_matched_tutor)) {
+            $matched_tname = $row_matched_tutor['TNAME'];
+        }
+        mysqli_free_result($result_matched_tutor); // 释放结果集
+    }
+    mysqli_free_result($result3); // 释放结果集
+}
+mysqli_free_result($result2); // 释放结果集
+
+mysqli_close($conn); // 关闭数据库连接
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>学生页面</title>
     <meta charset="zh_cn">
-<!--    <link rel="stylesheet" href="student_page.css">-->
     <style>
-        table{
+        table {
             width: 100%;
             border-collapse: collapse;
         }
-        th,td{
+        th, td {
             border: 1px solid black;
             padding: 8px;
             text-align: center;
         }
-        th{
+        th {
             background-color: #f2f2f2;
         }
-        tr:nth-child(even){
+        tr:nth-child(even) {
             background-color: #f2f2f2;
         }
-        tr:hover{
+        tr:hover {
             background-color: #f5f5f5;
         }
-        .application{
+        .application {
             border: 1px solid black;
             margin: 10px;
             padding: 10px;
@@ -108,48 +136,76 @@ mysqli_close($conn);
     </style>
 </head>
 <body>
-    <h1>选择导师</h1>
-    <form action="store_std_choice.php" method="POST">
-        <label for="sno">学号:</label>
-        <input type="text" name="sno" id="sno"><br>
-        <label for="sname">姓名:</label>
-        <input type="text" name="sname" id="sname"><br>
-        <label for="sdept">专业:</label>
-        <input type="text" name="sdept" id="sdept"><br>
-        <label for="first_tno">第一志愿教师编号:</label>
-        <input type="text" name="first_tno" id="first_tno"><br>
-        <label for="second_tno">第二志愿教师编号:</label>
-        <input type="text" name="second_tno" id="second_tno"><br>
-        <label for="third_tno">第三志愿教师编号:</label>
-        <input type="text" name="third_tno" id="third_tno"><br>
-        <input type="submit" value="提交">
-    </form>
-    <div>
+<h1>选择导师</h1>
+<form action="store_std_choice.php" method="POST">
+    <label for="sno">学号:</label>
+    <input type="text" name="sno" id="sno"><br>
+    <label for="sname">姓名:</label>
+    <input type="text" name="sname" id="sname"><br>
+    <label for="sdept">专业:</label>
+    <input type="text" name="sdept" id="sdept"><br>
+    <label for="first_tno">第一志愿教师编号:</label>
+    <input type="text" name="first_tno" id="first_tno"><br>
+    <label for="second_tno">第二志愿教师编号:</label>
+    <input type="text" name="second_tno" id="second_tno"><br>
+    <label for="third_tno">第三志愿教师编号:</label>
+    <input type="text" name="third_tno" id="third_tno"><br>
+    <input type="submit" value="提交">
+</form>
+<div>
     <h2>导师申请进度</h2>
-    <div class="application">
-        <div class="chosen_teacher"></div>
-
-    </div>
-    <div class="application"></div>
-    <div class="application"></div>
-    </div>
+    <div class="application" id="app_status"></div>
+    <div class="application" id="result_status"></div>
+    <div class="application" id="review_status"></div>
+    <div class="application" id="application_details"></div>
+</div>
 </body>
 
-<SCRIPT>
-    document.getElementById('sno').value="<?php echo $SNO;?>";
-    document.getElementById('sname').value="<?php echo $NAME;?>";
-    document.getElementById('sdept').value="<?php echo $DEPT;?>";
-    let application=<?php echo $application;?>;
-    let re_choose=<?php echo $re_choose;?>;
-    console.log(application);
-    console.log(re_choose);
-    if(application==1){
-        document.getElementsByClassName('application')[0].innerHTML='<h3>您的导师申请已提交</h3>';
+<script>
+    // 自动填写学生信息
+    document.getElementById('sno').value = "<?php echo $SNO; ?>";
+    document.getElementById('sname').value = "<?php echo $NAME; ?>";
+    document.getElementById('sdept').value = "<?php echo $DEPT; ?>";
 
-    }
-    if(re_choose==1){
-        document.getElementsByClassName('application')[1].innerHTML='<h3>导师申请成功</h3>';
-    }
+    // 设置申请状态和选择状态
+    let application = <?php echo $application; ?>;
+    let re_choose = <?php echo $re_choose; ?>;
+    let first_tno = "<?php echo $first_tno; ?>";
+    let second_tno = "<?php echo $second_tno; ?>";
+    let third_tno = "<?php echo $third_tno; ?>";
+    let first_tname = "<?php echo $first_tname; ?>";
+    let second_tname = "<?php echo $second_tname; ?>";
+    let third_tname = "<?php echo $third_tname; ?>";
+    let matched_tno = "<?php echo $matched_tno; ?>";
+    let matched_tname = "<?php echo $matched_tname; ?>";
 
-</SCRIPT>
-</html>
+    // 检查申请状态
+    if (application == 0) {
+        // 情况1: 学生尚未提交申请
+        document.getElementById('app_status').innerHTML = '<h3>您尚未提交导师申请</h3>';
+        document.getElementById('review_status').innerHTML = '<h3>请填写申请表并提交。</h3>';
+    } else {
+        // 情况2: 学生已提交申请
+        document.getElementById('app_status').innerHTML = '<h3>您的导师申请已提交</h3>';
+        document.getElementById('application_details').innerHTML = `
+            <h3>您申请的导师信息：</h3>
+            <p>第一志愿导师：编号 - ${first_tno}，姓名 - ${first_tname}</p>
+            <p>第二志愿导师：编号 - ${second_tno}，姓名 - ${second_tname}</p>
+            <p>第三志愿导师：编号 - ${third_tno}，姓名 - ${third_tname}</p>
+        `;
+
+        if (re_choose == 0) {
+            // 情况3: 学生已提交申请，但导师尚未选择
+            document.getElementById('review_status').innerHTML = '<h3>导师正在查看您的申请，请耐心等待......</h3>';
+            document.getElementById('result_status').innerHTML = '<h3>您目前还没有被导师选择。</h3>';
+        } else {
+            // 情况4: 学生已提交申请，且导师已选择
+            document.getElementById('review_status').innerHTML = `
+                <h3>导师选择信息：</h3>
+                <p>您已被导师选择：</p>
+                <p>导师编号 - ${matched_tno}，姓名 - ${matched_tname}</p>
+            `;
+            document.getElementById('result_status').innerHTML = '<h3>恭喜！您已被导师选择。</h3>';
+        }
+    }
+</script>
--- tutors_page.php
+++ tutors_page.php
@@ -1,92 +1,69 @@
-<!--//将学生的选择以表格形式输出-->
 <?php
 $pwd = file_get_contents('D:/PHP/file/S_T_ADMIN_SYS/src/pw.txt');
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pd = $pwd;
+$db_password = $pwd;
 $db_name = 'Academic_Tutor_Management_System';
-$conn = mysqli_connect($db_host, $db_user, $db_pd, $db_name) or die('Connected successfully');
-echo "Connected successfully";
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die('Database connection error');
 
-if (isset($_COOKIE['account'])) {
-    $account = htmlspecialchars($_COOKIE['account']);
-    echo "Welcome back, " . $account . "!";
-} else {
-    echo "Welcome, guest!";
-}
-echo $_COOKIE['copy_account'];
-//为什么存入的COOKie只有一个数字，而不是账号
-$sql = "SELECT SNO,SNAME,SDEPT FROM S_CHOICE_T WHERE FRIST_TNO=" . $_COOKIE['copy_account'];
+// 获取当前导师的编号
+$current_tutor_no = $_COOKIE['copy_account'];
+
+// 查询所有选择当前导师为第一、第二或第三志愿的学生
+$sql = "SELECT DISTINCT SCHOICES.SNO, SCHOICES.SNAME, SCHOICES.SDEPT, 
+        CASE 
+            WHEN SCHOICES.FRIST_TNO = '$current_tutor_no' THEN '第一志愿'
+            WHEN SCHOICES.SECOND_TNO = '$current_tutor_no' THEN '第二志愿'
+            WHEN SCHOICES.THIRD_TNO = '$current_tutor_no' THEN '第三志愿'
+        END AS CHOICE_TYPE
+        FROM S_CHOICE_T SCHOICES
+        WHERE '$current_tutor_no' IN (SCHOICES.FRIST_TNO, SCHOICES.SECOND_TNO, SCHOICES.THIRD_TNO)
+        AND NOT EXISTS (
+            SELECT 1 
+            FROM T_CHOICE_S T_CHOICES 
+            WHERE T_CHOICES.SNO = SCHOICES.SNO 
+            AND T_CHOICES.TNO = '$current_tutor_no'
+        )";
 $result = mysqli_query($conn, $sql);
-//将学生的选择以表格形式输出
-echo "<h2>第一选择为您的学生</h2><table border='1'>
-<tr>
-<th>学号</th>
-<th>姓名</th>
-<th>专业</th>
-<th colspan='2'>操作选项</th>
-</tr>";
-while ($row = mysqli_fetch_assoc($result)) {
-    $sql = "SELECT SNO,SNAME,SDEPT FROM T_CHOICE_S WHERE SNO=" . $row['SNO'];
-    $result2 = mysqli_query($conn, $sql);
-    if ($result2 == false) {
-        echo "<tr>";
-        echo "<td>" . $row['SNO'] . "</td>";
-        echo "<td>" . $row['SNAME'] . "</td>";
-        echo "<td>" . $row['SDEPT'] . "</td>";
-        echo "<td><button>查看学生信息</button></td>";
-        echo "<td><button class='t_choice_s'>选择他/她</button></td>";
-        echo "</tr>";
-    }
-}
-echo "</table>";
-$sql0 = "SELECT SNO,SNAME,SDEPT FROM S_CHOICE_T WHERE SECOND_TNO=" . $_COOKIE['copy_account'];
-$result0 = mysqli_query($conn, $sql0);
-//将学生的选择以表格形式输出
-echo "<h2>第二选择为您的学生</h2><table border='1'>
-<tr>
-<th>学号</th>
-<th>姓名</th>
-<th>专业</th>
-<th colspan='2'>操作选项</th>
-</tr>";
-while ($row = mysqli_fetch_assoc($result0)) {
-    $sql = "SELECT SNO,SNAME,SDEPT FROM T_CHOICE_S WHERE SNO=" . $row['SNO'];
-    $result3 = mysqli_query($conn, $sql);
-    if ($result3 == false) {
-        echo "<tr>";
-        echo "<td>" . $row['SNO'] . "</td>";
-        echo "<td>" . $row['SNAME'] . "</td>";
-        echo "<td>" . $row['SDEPT'] . "</td>";
-        echo "<td><button>查看学生信息</button></td>";
-        echo "<td><button class='t_choice_s'>选择他/她</button></td>";
-        echo "</tr>";
-    }
-}
-echo "</table>";
-$sql1 = "SELECT SNO,SNAME,SDEPT FROM S_CHOICE_T WHERE THIRD_TNO=" . $_COOKIE['copy_account'];
-$result1 = mysqli_query($conn, $sql1);
-//将学生的选择以表格形式输出
-echo "<h2>第三选择为您的学生</h2><table border='1'>
-<tr>
-<th>学号</th>
-<th>姓名</th>
-<th>专业</th>
-<th colspan='2'>操作选项</th>
-</tr>";
-while ($row = mysqli_fetch_assoc($result1)) {
-    $sql = "SELECT SNO,SNAME,SDEPT FROM T_CHOICE_S WHERE SNO=" . $row['SNO'];
-    $result4 = mysqli_query($conn, $sql);
 
-    if ($result4 == false) {
-        echo "<tr>";
-        echo "<td>" . $row['SNO'] . "</td>";
-        echo "<td>" . $row['SNAME'] . "</td>";
-        echo "<td>" . $row['SDEPT'] . "</td>";
-        echo "<td><button>查看学生信息</button></td>";
-        echo "<td><button class='t_choice_s'>选择他/她</button></td>";
-        echo "</tr>";
-    }
+// 输出学生选择表格
+echo "<h2>选择您为导师的学生</h2><table border='1'>
+<tr>
+<th>学号</th>
+<th>姓名</th>
+<th>专业</th>
+<th>志愿类型</th>
+<th>操作选项</th>
+</tr>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['SNO'] . "</td>";
+    echo "<td>" . $row['SNAME'] . "</td>";
+    echo "<td>" . $row['SDEPT'] . "</td>";
+    echo "<td>" . $row['CHOICE_TYPE'] . "</td>";
+    echo "<td><button class='t_choice_s'>选择他/她</button></td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+// 查询已经选择的学生
+$sql_selected = "SELECT SNO, SNAME, SDEPT FROM T_CHOICE_S WHERE TNO='$current_tutor_no'";
+$result_selected = mysqli_query($conn, $sql_selected);
+
+echo "<h2>您已选择的学生</h2><table border='1'>
+<tr>
+<th>学号</th>
+<th>姓名</th>
+<th>专业</th>
+</tr>";
+
+while ($row = mysqli_fetch_assoc($result_selected)) {
+    echo "<tr>";
+    echo "<td>" . $row['SNO'] . "</td>";
+    echo "<td>" . $row['SNAME'] . "</td>";
+    echo "<td>" . $row['SDEPT'] . "</td>";
+    echo "</tr>";
 }
 echo "</table>";
 
@@ -99,66 +76,42 @@ mysqli_close($conn);
     <title>导师页面</title>
 </head>
 <body>
-
-</body>
-<!--点击按钮选择学生-->
+<!-- Ajax 脚本 -->
 <script>
-    //为按钮绑定事件
+    // 为按钮绑定事件
     var store_T_S = document.getElementsByClassName('t_choice_s');
-    //当点击按钮时，将学生的选择存入数据库
+
     for (let i = 0; i < store_T_S.length; i++) {
         store_T_S[i].onclick = function () {
-            let sname = this.parentNode.parentNode.children[1].innerText;
-            let sno = this.parentNode.parentNode.children[0].innerText;
-            let sdept = this.parentNode.parentNode.children[2].innerText;
+            let row = this.parentNode.parentNode;
+            let sno = row.children[0].innerText;
+            let sname = row.children[1].innerText;
+            let sdept = row.children[2].innerText;
+
             var xhr = new XMLHttpRequest();
             xhr.open('POST', 'store_t_s_choice.php', true);
             xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            xhr.send('sname=' + sname + '&sno=' + sno + '&sdept=' + sdept);
+            xhr.send('sno=' + sno + '&sname=' + sname + '&sdept=' + sdept);
+
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     console.log(xhr.responseText);
+                    // 将按钮状态更新为“已选择”
+                    store_T_S[i].innerText = '已选择';
+                    store_T_S[i].disabled = true;
                 }
             }
         }
     }
-    //按钮点击后，不可再次点击，即使刷新页面也不可再次点击
-    for (let i = 0; i < store_T_S.length; i++) {
-        store_T_S[i].onclick = function () {
-            this.innerText = '已选择';
-            this.disabled = true;
+
+    // 页面加载时禁用已经选择的按钮
+    window.onload = function() {
+        for (let i = 0; i < store_T_S.length; i++) {
+            if (store_T_S[i].innerText == '已选择') {
+                store_T_S[i].disabled = true;
+            }
         }
     }
-
 </script>
-<!--//查询已选择的学生-->
-<?php
-$pwd = file_get_contents('D:/PHP/file/S_T_ADMIN_SYS/src/pw.txt');
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = $pwd;
-$db_name = 'Academic_Tutor_Management_System';
-$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die('Database connection error');
-$sql = "SELECT SNO,SNAME,SDEPT FROM T_CHOICE_S WHERE TNO=" . $_COOKIE['copy_account'];
-$result = mysqli_query($conn, $sql);
-echo "<h2>您已选择的学生</h2><table border='1'>
-        <tr>
-        <th>学号</th>
-        <th>姓名</th>
-        <th>专业</th>
-        </tr>";
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>" . $row['SNO'] . "</td>";
-    echo "<td>" . $row['SNAME'] . "</td>";
-    echo "<td>" . $row['SDEPT'] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
-if ($result) {
-    echo "查询成功";
-} else {
-    echo "查询失败";
-}
-?>
+</body>
 </html>
